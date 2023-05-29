ffda4756ff1259581bfc8fb49856eebc1e10f7e2
michalplebanski <32018412+michalplebanski@users.noreply.github.com>
2023-05-29 23:49:39 +0200

    Update palindrom function

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def check_palindrom(name):
-  if name.isalnum():
-    name = name.lower().replace(" ", "")
-    return name == name[::-1]  
-  
-status = check_palindrom("łapał")
-print(status)
+def check_palindrom(text):
+  chars = [x for x in text.lower() if x.isalnum()]
+  return chars == chars[::-1]
+
+texts = ["ała", "łapał", "Ikar łapał raki!"]
+
+for text in texts:
+  status = check_palindrom(text)
+  print(status)
